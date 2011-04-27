8d9b5c2b56adbdbed0eecd5d7ab8d0b7c7977928
Andrew <drewvergara@gmail.com>
2011-04-27 10:15:42 -0700

    Adding in a view if some doesnt have wireless connection

---
+++ Classes/SelectedViewController.m
@@ -0,0 +1,70 @@
+//
+//  SelectedViewController.m
+//  TED
+//
+//  Created by Andrew Vergara on 4/27/11.
+//  Copyright 2011 Studio Noodlehouse. All rights reserved.
+//
+
+#import "SelectedViewController.h"
+
+
+@implementation SelectedViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [super viewDidLoad];
+    
+	self.navigationController.navigationBar.hidden = NO;
+    
+	UIImageView *imgView = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"TED-logo-sm.png"]] autorelease];
+	[imgView setContentMode:UIViewContentModeScaleAspectFit];
+	[imgView setFrame:CGRectMake(0, 0, 50, 17.5)];
+	self.navigationItem.titleView = imgView;	
+	
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+   //self.navigationItem.leftBarButtonItem = self.
+    
+        
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
